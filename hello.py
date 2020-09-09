2a049652811e393f2e5a1d65af06abec4a3aef12
Dayong <dayong@Dayongs-iMac.local>
2020-09-08 23:40:56 -0700

    add class files

--- hello.py
+++ hello.py
@@ -1,23 +1,62 @@
 
 
 # basic output fucntion, print string
-print ("hello")
+# print ("hello disney")
 
-# variable assignment
-x = 1
-print (x)
+# # variable assignment
 
-# input function
-y = input("test")
-
-
-# indentation
-# x = 1
-    y = 2
-print (x)
-print (y)
-
+# x = 2 + 2
+# print (x)
+#
+# x = 2 + x
+# print (x)
 
 # formatted output
-print ("I got this %s" % x)
+# x = 2
+# x= "hello %s" % x
+# print (x)
 
+# temperature = 72
+# print ("today's temperature is %s" % temperature)
+
+
+# x = input("What is today's temperature? ")
+# print (x)
+
+
+c = input("Input temperature in Celsius: ")
+
+print ("Current Celsius %s" % c)
+
+f = int(c) * 9 / 5 + 32
+
+print ("Current Fahrenheit %s" % f)
+
+
+# x = "hello" + 2
+#
+# # input function
+# y = input("test")
+#
+#
+# # indentation
+# # x = 1
+#
+#
+# y = 2
+# print (x)
+# print (y)
+#
+#
+# # formatted output
+# print ("I got this %s" % x)
+#
+#
+# # conditionals
+#
+# t = 1
+#
+# if t == 1:
+#     print ("t is 1")
+# else:
+#     print ("t is not 1")
--- temp_converter_conditional.py
+++ temp_converter_conditional.py
@@ -2,22 +2,41 @@
 
 # conditional and indentation
 
-t = 1
-
-if t == 1:
-    print ("t is 1")
-else:
-    print ("t is not 1")
-
-
-# t = input("Input temperature: ")
+# t = 1
 #
-# if t[-1] == 'f':
-#     print ("got fahrenheit temperature %s degree" % t[:-1])
-# elif t[-1] == 'c':
-#     print ("got celsius temperature %s degree" % t[:-1])
+# if t == 1:
+#     print ("t is 1")
 # else:
-#     print ("Temperature must end with c or f!")
+#     print ("t is not 1")
+
+
+# Homework 2:
+'''
+$ python temperature_converter.py
+Input temperature: 2f
+Got fahrenheit temperature 2 degree
+The fahrenheit temperature 2 degree in celsius degree is -16.666666666666668
+
+
+$ python temperature_converter.py
+Input temperature: 23c
+Got celsius temperature 23 degree
+The celsius temperature 23 degree in fahrenheit degree is 73.4
+'''
+
+
+t = input("Input temperature: ")
+
+if t[-1] == 'f':
+    print ("Got fahrenheit temperature %s degree" % t[:-1])
+    c = (int(t[:-1]) - 32) * 5 / 9
+    print ("The fahrenheit temperature %s degree in celsius degree is %s " % (t[:-1], c))
+elif t[-1] == 'c':
+    print ("Got celsius temperature %s degree" % t[:-1])
+    f = int(t[:-1]) * 9 / 5 + 32
+    print ("The celsius temperature %s degree in fahrenheit degree is %s " % (t[:-1], f))
+else:
+    print ("Temperature must end with c or f!")
 
 
 # assignment one
