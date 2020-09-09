6f64135195b78a2fe91ebc1b06bdfbd87fc1b2d0
Dayong <dayong@Dayongs-iMac.local>
2020-09-09 00:55:31 -0700

    add class one assignments

---
+++ class_one/assignment_one.py
@@ -0,0 +1,20 @@
+
+
+### The following code converts temperature from Celsius to Fahrenheit. Develop a convert python script to
+# convert temperature from Fahrenheit to Celsius
+
+
+c = input("Input temperature in Celsius: ")
+
+print ("Current Celsius %s" % c)
+
+f = int(c) * 9 / 5 + 32
+
+print ("Current Fahrenheit %s" % f)
+
+# For your assignment:
+# Sample input:
+# Expected output:
+# Input temperature in Fahrenheit: 2
+# Current Fahrenheit: 2
+# Current Celsius: -16.666666666666668
---
+++ class_one/assignment_two.py
@@ -0,0 +1,21 @@
+
+
+# Assignment 2:
+# This assignement is intended for advanced student.
+#
+# Develop a program, takes user's input. If the user's input is a number ending with "f", convert the user input to
+# from Fahrenheit to Celcius. If the user's input is a number ending with "c", convert user input from celsius to Fahrenheit.
+
+'''
+$ python assignment_two.py
+Input temperature: 2f
+Got Fahrenheit temperature 2 degree
+The Fahrenheit temperature 2 degree in Celsius degree is -16.666666666666668
+
+
+$ python assignment_two.py
+Input temperature: 23c
+Got Celsius temperature 23 degree
+The Celsius temperature 23 degree in Fahrenheit degree is 73.4
+'''
+
