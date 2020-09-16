0491706547b74739e994ba0a4831da39bd46b49c
Dayong Huang <dayhuang@gmail.com>
2020-09-16 00:22:30 -0700

    class two content and assignment

---
+++ class_two/assignment_two.py
@@ -0,0 +1,20 @@
+
+
+# Assignment 2:
+#
+# Develop a program, takes user's input. If the user's input is a string starts with "f", convert the user input to
+# from Fahrenheit to Celcius. For example, f23 denotes Fahrenheit degree of 23. The output should be the Celsius conversion of Fahrenheit 23 degrees. If the user's input is a string starts with "c", convert user input from celsius to Fahrenheit.
+
+'''
+$ python assignment_two.py
+Input temperature: f2
+Got Fahrenheit temperature 2 degree
+The Fahrenheit temperature 2 degree in Celsius degree is -16.666666666666668
+
+
+$ python assignment_two.py
+Input temperature: c23
+Got Celsius temperature 23 degree
+The Celsius temperature 23 degree in Fahrenheit degree is 73.4
+'''
+
---
+++ class_two/conditional_and_string.py
@@ -0,0 +1,48 @@
+# f = int(input("Input temperature in Fahrenheit: "))
+#
+# print ("Current Fahrenheit: %s" % f)
+#
+#
+# # conditional if
+#
+# high = 90
+#
+# low = 32
+#
+# superhot = 1000000
+#
+# if f > superhot:
+#     print ("this is superhot")
+# elif f < low:
+#     print ("this is cold")
+# elif f > high:
+#     print ("this is too hot")
+# else:
+#     print ("today's temperature is okay")
+
+
+
+x = "hello class this is a really really long sentence and this goes on and on"
+
+print (len(x))
+
+y = x[0]
+print (y)
+y = x[0:5]
+print (y)
+y = x[6:11]
+print (y)
+
+
+
+
+# print (x)
+# y = str(input("please tell me your name: "))
+# print (y)
+#
+# z = int(input("how old are you"))
+# print (z)
+#
+# print (y + z)
+
+
