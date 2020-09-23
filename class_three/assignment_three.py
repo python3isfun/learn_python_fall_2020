954f1179480c76ac9662b4383c57b89bed978339
Dayong <dayong@Dayongs-iMac.local>
2020-09-23 00:48:24 -0700

    add assignment

---
+++ class_three/assignment_three.py
@@ -0,0 +1,17 @@
+# You are given a input file called temperatures.txt.
+# In this file there are multiple lines.
+# each line has some numbers.
+# Your task is read this file, and provide three outputs of all the numbers.
+# The smallest number in the file, the largest number in the file, and the average of all numbers in the file.
+
+# example file
+
+# 1 2 3
+# 1
+# 4
+
+# In the above example, there are three lines, totally five numbers in the file.
+# the output after processing the input file should be printed as:
+# the smallest number is 1.
+# the largest number is 4
+# the average of all numbers is 2.2
